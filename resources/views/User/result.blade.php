8cd0a0b0bc957e1686e67debf5f25f052fafd1d3
Akram Hossain <35065634+Coder-fox-bd@users.noreply.github.com>
2019-11-24 13:02:01 +0600

    7

--- resources/views/User/result.blade.php
+++ resources/views/User/result.blade.php
@@ -28,7 +28,7 @@
                 </div>
 
                 @php
-                    $i =0;
+                    $i=0;
                 @endphp
                 @foreach($matchs as $match)
                     @php
@@ -66,95 +66,9 @@
                                 }
                             });
                         })
-                        jQuery(document).ready(function(){
-                            jQuery('#search{{$i}}').on('click', function(event) {
-                                jQuery('result{{$i}}').toggle('show');
-                            });
-                        });
                     </script>
                 @endforeach
-
-
-
-
-
-
-
-{{--                <div class="row">--}}
-{{--                    <div class="col-12 pt-3 text-center bg-white">--}}
-{{--                        <h6><strong>Solo Evening #112(Mobile)</strong></h6>--}}
-{{--                        <p>Organized on</p>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="row mt-3 pt-3 pb-3 bg-white">--}}
-{{--                    <div class="col-4 p-0 text-center">--}}
-{{--                        <p>Win Prize</p>--}}
-{{--                        <strong>100</strong>--}}
-{{--                    </div>--}}
-{{--                    <div class="col-4 p-0 text-center">--}}
-{{--                        <p>Per Kill</p>--}}
-{{--                        <strong>10</strong>--}}
-{{--                    </div>--}}
-{{--                    <div class="col-4 p-0 text-center">--}}
-{{--                        <p>Entry Fee</p>--}}
-{{--                        <strong>20</strong>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="row mt-3 bg-yellow">--}}
-{{--                    <div class="col-12 pt-1 pb-1 text-center">--}}
-{{--                        <strong>WINNER OF THE MATCH</strong>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="row mt-2">--}}
-{{--                    <div class="col-12 p-0 mt-2">--}}
-{{--                        <table class="table table-light text-center">--}}
-{{--                            <thead class="thead-dark border-0">--}}
-{{--                            <tr>--}}
-{{--                                <th scope="col">#</th>--}}
-{{--                                <th scope="col">Player Name</th>--}}
-{{--                                <th scope="col">Kills</th>--}}
-{{--                                <th scope="col">Winning</th>--}}
-{{--                            </tr>--}}
-{{--                            </thead>--}}
-{{--                            <tbody>--}}
-{{--                            <tr>--}}
-{{--                                <th scope="row">1</th>--}}
-{{--                                <td>bfsfdhk</td>--}}
-{{--                                <td>7</td>--}}
-{{--                                <td>130</td>--}}
-{{--                            </tr>--}}
-{{--                            </tbody>--}}
-{{--                        </table>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="row mt-2 bg-yellow">--}}
-{{--                    <div class="col-12 pt-1 pb-1 text-center">--}}
-{{--                        <strong>FULL RESULT</strong>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="row mt-2">--}}
-{{--                    <div class="col-12 p-0 mt-2">--}}
-{{--                        <table class="table table-light text-center">--}}
-{{--                            <thead class="thead-dark border-0">--}}
-{{--                            <tr>--}}
-{{--                                <th scope="col">#</th>--}}
-{{--                                <th scope="col">Player Name</th>--}}
-{{--                                <th scope="col">Kills</th>--}}
-{{--                                <th scope="col">Winning</th>--}}
-{{--                            </tr>--}}
-{{--                            </thead>--}}
-{{--                            <tbody>--}}
-{{--                                <tr>--}}
-{{--                                    <th scope="row"></th>--}}
-{{--                                    <td>bfsfdhk</td>--}}
-{{--                                    <td>7</td>--}}
-{{--                                    <td>130</td>--}}
-{{--                                </tr>--}}
-{{--                            </tbody>--}}
-{{--                        </table>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
+            </div>
         </div>
     </div>
 
